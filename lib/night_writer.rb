86204d449ca3daa15f3766df9455b9d45df79d23
Shirley DeCesari <shirley.decesari@gmail.com>
2022-04-23 11:26:35 -0700

    Add test and file for night writer

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,2 +1,13 @@
-# $ ruby ./lib/night_writer.rb message.txt braille.txt
-# Created 'braille.txt' containing 256 characters
+
+input_reader = File.open(ARGV[0], "r")
+message_reader = input_reader.read.strip
+input_reader.close
+
+
+chars = message_reader.length
+
+output_writer = File.open(ARGV[1], "w")
+output_writer.write(chars)
+output_writer.close
+
+puts "Created '#{ARGV[1]}' containing #{chars} characters"
