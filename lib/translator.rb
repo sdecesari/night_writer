db5348a447c9a805599a940e29ad20286752c209
Shirley DeCesari <shirley.decesari@gmail.com>
2022-04-26 13:56:00 -0700

    Add method for translator

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,36 +7,11 @@ class Translator
     @char_length = message_reader.length
   end
 
-  # def character_split
-  #   message_chunks = @message_array.each_slice(40).map(&:join)
-  #   dictionary = Dictionary.new
-  #   message_chunks.each do |chunk|
-  #     @letter_string << chunk.split("")
-  #   end
-  #   braille_array
-  # end
-
   def translate
-    message_array = @message_reader.split("")
-    dictionary = Dictionary.new
-    message_array.map do |letter|
-      dictionary.braille_alphabet[letter]
+  message_array = @message_reader.split("")
+  dictionary = Dictionary.new
+  message_array.map do |letter|
+    dictionary.braille_alphabet[letter]
     end
-  end
-
-  def formater
-    column1 = []
-    column2 = []
-    column3 = []
-    translate.each do |array|
-      column1.push(array[0].to_s)
-      column2.push(array[1].to_s)
-      column3.push(array[2].to_s)
-    end
-    "#{column1.join}\n#{column2.join}\n#{column3.join}\n"
-  end
-
-  # def character split
-  #   formater.char_length.each_slice(40).map(&:join).join
-  # end
+  end 
 end
