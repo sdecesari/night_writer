f00f1218f1b651012494ce6ab5f4bfbaa0cdfa63
Shirley DeCesari <shirley.decesari@gmail.com>
2022-04-26 14:16:11 -0700

    Add method for formater

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,10 +8,23 @@ class Translator
   end
 
   def translate
-  message_array = @message_reader.split("")
-  dictionary = Dictionary.new
-  message_array.map do |letter|
-    dictionary.braille_alphabet[letter]
+    message_array = @message_reader.split("")
+    dictionary = Dictionary.new
+    message_array.map do |letter|
+      dictionary.braille_alphabet[letter]
     end
-  end 
+  end
+
+  def formater
+    column1 = []
+    column2 = []
+    column3 = []
+    translate.each do |array|
+      column1.push(array[0].to_s)
+      column2.push(array[1].to_s)
+      column3.push(array[2].to_s)
+    end
+    "#{column1.join}\n#{column2.join}\n#{column3.join}\n"
+  end
+
 end
