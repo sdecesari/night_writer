39d87d074dfb8dd528ff753796eb8c56e0690041
Shirley DeCesari <shirley.decesari@gmail.com>
2022-04-26 16:10:07 -0700

    Remove comments from translator

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,20 +1,9 @@
-class Translator
-
-  attr_reader :message_reader, :char_length
+require_relative 'dictionary'
+class Translator < Dictionary
 
   def initialize(message_reader)
     @message_reader = message_reader
-    @char_length = message_reader.length
   end
-
-  # def character_split
-  #   message_chunks = @message_array.each_slice(40).map(&:join)
-  #   dictionary = Dictionary.new
-  #   message_chunks.each do |chunk|
-  #     @letter_string << chunk.split("")
-  #   end
-  #   braille_array
-  # end
 
   def translate
     message_array = @message_reader.split("")
@@ -36,7 +25,4 @@ class Translator
     "#{column1.join}\n#{column2.join}\n#{column3.join}\n"
   end
 
-  # def character split
-  #   formater.char_length.each_slice(40).map(&:join).join
-  # end
 end
