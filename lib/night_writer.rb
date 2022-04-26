94a08f2da518bf32baba4974f903ffabccd66fbc
Shirley DeCesari <shirley.decesari@gmail.com>
2022-04-26 16:08:58 -0700

    Remove comments from night writer

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -6,11 +6,11 @@ input_reader = File.open(ARGV[0], "r")
 message_reader = input_reader.read.strip
 input_reader.close
 translator = Translator.new(message_reader)
+@char_length = message_reader.length
 
 
 output_writer = File.open(ARGV[1], "w")
 output_writer.write(translator.formater)
 output_writer.close
-# binding.pry
-puts "Created '#{ARGV[1]}' containing #{translator.char_length} characters"
-# puts translator.formater
+
+puts "Created '#{ARGV[1]}' containing #{@char_length} characters"
