7ab43700448b4c3c7d0ebeb57e692bd9be35a84c
Shirley DeCesari <shirley.decesari@gmail.com>
2022-04-26 14:32:18 -0700

    Update night writer unsure of changes

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,16 +1,16 @@
 require './lib/dictionary'
 require './lib/translator'
+require 'pry'
 
 input_reader = File.open(ARGV[0], "r")
 message_reader = input_reader.read.strip
 input_reader.close
+translator = Translator.new(message_reader)
 
-
-chars = message_reader.length
 
 output_writer = File.open(ARGV[1], "w")
-output_writer.write(message_reader)
+output_writer.write(translator.formater)
 output_writer.close
-
-puts "Created '#{ARGV[1]}' containing #{chars} characters"
-# puts message_reader
+# binding.pry
+puts "Created '#{ARGV[1]}' containing #{translator.char_length} characters"
+# puts translator.formater
