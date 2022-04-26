16d710ddc6e6d41201b438843128ac713d5bf234
Shirley DeCesari <shirley.decesari@gmail.com>
2022-04-26 16:07:44 -0700

    Update dictionary test

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -9,8 +9,11 @@ RSpec.describe Dictionary do
     @dictionary = Dictionary.new
   end
 
-  it "exists and has attributes" do
+  it "exists" do
     expect(@dictionary).to be_a Dictionary
+  end
+
+  it "has attributes" do
     expect(@dictionary.braille_alphabet.class).to eq(Hash)
   end
 end
